68b00a17cb9fcfa45cf95a31d0d983c9fb1fa417
Chetan Sarva <chetan@pixelcop.net>
2014-03-22 18:15:11 -0400

    fixed gpgme configure

--- config/software/gpgme.rb
+++ config/software/gpgme.rb
@@ -9,14 +9,20 @@ source :url => "ftp://ftp.gnupg.org/gcrypt/gpgme/gpgme-#{version}.tar.bz2",
 
 relative_path "gpgme-#{version}"
 
+prefix = "#{install_dir}/embedded"
 configure_env = {
-  "LDFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
-  "CFLAGS" => "-L#{install_dir}/embedded/lib -I#{install_dir}/embedded/include",
-  "LD_RUN_PATH" => "#{install_dir}/embedded/lib"
+  "LDFLAGS" => "-L#{prefix}/lib -I#{prefix}/include",
+  "CFLAGS" => "-L#{prefix}/lib -I#{prefix}/include",
+  "LD_RUN_PATH" => "#{prefix}/lib"
 }
 
 build do
-  command "./configure --prefix=#{install_dir}/embedded", :env => configure_env
-  command "make -j #{max_build_jobs}", :env => {"LD_RUN_PATH" => "#{install_dir}/embedded/lib"}
+  command ["./configure",
+           "--prefix=#{prefix}",
+           "--with-libgpg-error-prefix=#{prefix}",
+           "--with-libassuan-prefix=#{prefix}",
+           "--with-gpgconf=#{prefix}/bin/gpgconf"
+           ].join(" "), :env => configure_env
+  command "make -j #{max_build_jobs}", :env => {"LD_RUN_PATH" => "#{prefix}/lib"}
   command "make install"
 end
